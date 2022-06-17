3ee5f7adaf411a6bcd057b8fc5f48970cb043d26
yairelmaliah <yairelmaliah319732@gmail.com>
2022-06-17 15:36:49 +0300

    clean up

--- app/run.py
+++ app/run.py
@@ -1,54 +1,44 @@
-from flask import request, render_template, redirect, jsonify
+from flask import request, render_template, redirect
 from db import mysql
-import pymysql
 from app import app
+import pymysql
 from datetime import datetime
 import socket   
-import os
 
+# Get container IP
 hostname=socket.gethostname()   
 IPAddr=socket.gethostbyname(hostname)  
 
-dir = "/tmp/flask-chatapp-yair"
-try:
-  os.mkdir(dir)
-except:
-  pass
-
-
-
-
+# General room
 @app.route('/')
-def home_route():
+def general_room_route():
   return render_template('index.html', ip=IPAddr)
 
-
+# Any room
 @app.route('/<name>')
 def room_route(name):
   return render_template('index.html', ip=IPAddr)
 
+# Chat room
 @app.route('/api/chat/<room_name>', methods=['POST', 'GET'])
 def create_message(room_name):
   if request.method == 'POST':
-
+    
+    # Extract message data 
     data = dict(request.form)
-
     data['room_name'] = room_name
     data['time'] = datetime.now()
 
-    print(data, flush=True)
-
-    sql = "INSERT INTO `rooms` (`room_name`, `username`, `msg`, `msg_date`) VALUES (%s, %s, %s, %s)"
+    # Insert room message to db
     conn = mysql.connect()
-    
     cursor = conn.cursor(pymysql.cursors.DictCursor)
-    
+    sql = "INSERT INTO `rooms` (`room_name`, `username`, `msg`, `msg_date`) VALUES (%s, %s, %s, %s)"
     cursor.execute(sql, (room_name, data['username'], data['msg'], data['time']))
-
     conn.commit()
 
     return {"msg":"success", "status_code": 200}, 200
 
+  # For getting all messages redirect
   if request.method == 'GET':
     return redirect(f'/chat/{room_name}')
 
@@ -58,15 +48,16 @@ def create_message(room_name):
 def get_room(room_name=""):
   
   if not room_name:
-    room_name = "global"
+    room_name = "general"
   
+  # Fetch room messages
   conn = mysql.connect()
   cursor = conn.cursor(pymysql.cursors.DictCursor)
   sql = "SELECT * FROM rooms WHERE room_name=%s"
   cursor.execute(sql, (room_name,))
-
   res = cursor.fetchall()
   
+  # Get numbers of romm messages
   res_len = len(list(res))
 
   if not res:
--- init-chatapp.sh
+++ init-chatapp.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Check if user can 
 if ! command -v docker-compose &> /dev/null
 then
     echo "docker-compose could not be found in this os, please download it using below command"
-    echo "apt-get update -y; apt-get install docker-compose"
+    echo "sudo apt-get update -y; sudo apt-get install docker-compose"
     exit 1
 fi
 
+# Run containers
 docker-compose up --scale app=3 -d --build
 
+echo -e "\n====================="
 echo "Chat app is running..."
 echo "please go to - http://localhost to start using this little app :))" 
+echo -e "====================="
 
 exit 0
